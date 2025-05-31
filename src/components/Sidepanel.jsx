9a39fc3d375f8036f242a5911d6d984e3446e459
CheraHamza <cherahamza1@gmail.com>
2025-05-31 16:01:12 +0100

    feat: add Sidepanel component with form and image picker

---
+++ src/components/Sidepanel.jsx
@@ -0,0 +1,62 @@
+import img_placeholder_bg from "../../public/img_holder_w_bg.svg";
+import "../styles/Sidepanel.css";
+
+function Sidepanel() {
+	return (
+		<div className="sidepanel">
+			<h1 className="title">Personal Information</h1>
+			<ImagePicker img={img_placeholder_bg}></ImagePicker>
+			<Form></Form>
+		</div>
+	);
+}
+
+function Form() {
+	return (
+		<div className="form">
+			<Input label={"First Name"} type={"text"}></Input>
+			<Input label={"Last Name"} type={"text"}></Input>
+			<Input label={"Email"} type={"email"}></Input>
+			<Input label={"Phone Number"} type={"tele"}></Input>
+			<Input label={"Location"} type={"text"}></Input>
+			<TextArea label={"Bio"}></TextArea>
+		</div>
+	);
+}
+
+function Input({ label, type }) {
+	return (
+		<>
+			<label>
+				{label}
+				<input type={type} />
+			</label>
+		</>
+	);
+}
+
+function TextArea({ label }) {
+	return (
+		<>
+			<label>
+				{label}
+				<textarea></textarea>
+			</label>
+		</>
+	);
+}
+
+function ImagePicker({ img }) {
+	return (
+		<div className="imgPicker">
+			<div>
+				<button className="changebtn">Change</button>
+				<span>|</span>
+				<button className="deletebtn">Delete</button>
+			</div>
+			<img src={img} alt="profile picture" />
+		</div>
+	);
+}
+
+export default Sidepanel;
