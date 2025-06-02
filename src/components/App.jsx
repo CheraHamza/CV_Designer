a4427320be3893689e2811d4edf24a4c438c4af6
CheraHamza <cherahamza1@gmail.com>
2025-06-02 16:07:21 +0100

    feat: create App component to integrate Editor and Previewsection

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Previewsection from "./Previewsection.jsx";
+import Editor from "./Editor.jsx";
+
+function App() {
+	return (
+		<>
+			<Editor></Editor>
+			<Previewsection></Previewsection>
+		</>
+	);
+}
+
+export default App;
